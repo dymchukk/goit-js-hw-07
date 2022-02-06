e207e4d4c2aba234a5d56b96dea3e64903f5a3fd
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-06 10:17:17 +0200

    git commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);   
+    
+galleryContainer.addEventListener('click', onGalleryContainerClick);    
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div >
+                `;
+            })
+        .join('');
+}
+ 
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    } 
+    
+  modalShow(evt.target.dataset.source);
+}
+
+let instance;
+function modalShow(src) {
+  instance = basicLightbox.create(
+    `<img src="${src}"></img>`,
+    {
+      onShow: instance => {
+        addListener();
+      },
+      onClose: instance => {
+        removeListener();
+      },
+    },
+  );
+  instance.show();
+}
+
+function addListener() {
+  window.addEventListener('keydown', onEscClick);
+}
+
+function onEscClick(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+}
+
+function removeListener() {
+  window.removeEventListener('keydown', onEscClick);
+}
